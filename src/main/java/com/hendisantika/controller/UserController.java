185b3a684e31ff5e39a8970e5956e0144677d060
Hendi Santika <hendisantika@gmail.com>
2021-10-12 19:27:31 +0700

    #31 - Add UserController

---
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-reactive-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/21
+ * Time: 19.27
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
