05ed820efe88a705ea3a8ee22cd6f38e971f5ce6
Hendi Santika <hendisantika@gmail.com>
2021-10-12 19:18:55 +0700

    #9 - Add UserService

---
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -0,0 +1,30 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.DepartmentRepository;
+import com.hendisantika.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-reactive-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/21
+ * Time: 19.18
+ */
+@Service
+@Slf4j
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+}
