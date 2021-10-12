bf136a700120c75e3a974165bcdbc903f9beca33
Hendi Santika <hendisantika@gmail.com>
2021-10-12 19:28:26 +0700

    #33 - Add getAllUsers in UserController

--- src/main/java/com/hendisantika/controller/UserController.java
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -4,11 +4,13 @@ import com.hendisantika.model.User;
 import com.hendisantika.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -30,5 +32,10 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<User> create(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping
+    public Flux<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
