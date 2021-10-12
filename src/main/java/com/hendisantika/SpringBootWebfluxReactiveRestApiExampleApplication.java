01bd139e4ad1e011eef3c83a24802e20aee7ca9d
Hendi Santika <hendisantika@gmail.com>
2021-10-12 19:11:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootWebfluxReactiveRestApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxReactiveRestApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxReactiveRestApiExampleApplication.class, args);
+    }
+
+}
