8013a65178cf448bd6dfdbae666f458c4bd9f8ae
Hendi Santika <hendisantika@gmail.com>
2021-10-12 19:23:33 +0700

    #21 - Add CustomConnectionFactoryInitializer

---
+++ src/main/java/com/hendisantika/config/CustomConnectionFactoryInitializer.java
@@ -0,0 +1,31 @@
+package com.hendisantika.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-reactive-rest-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/21
+ * Time: 19.23
+ */
+@Configuration
+public class CustomConnectionFactoryInitializer {
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+}
